0f7bead07a9ff6c93a7ffebc01b3816c1a30bdd9
miss-ping <79873852+miss-ping@users.noreply.github.com>
2021-06-09 09:40:59 +0100

    Initial set up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary app</h1>
+        <p>Type a word to look up</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
