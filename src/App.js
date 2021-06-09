66dd59e8a9ac66c8e8cec20746299b6f0b97b7b6
miss-ping <79873852+miss-ping@users.noreply.github.com>
2021-06-09 22:32:18 +0100

    Added Dictionary search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary app</h1>
-        <p>Type a word to look up</p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary app</h1>
+          <p>Type a word to look up</p>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center App-footer">
+          <a href="https://github.com/miss-ping/dictionary-app">Open source </a>
+          coded by Sílvia Ping
+        </footer>
+      </div>
     </div>
   );
 }
